3130c11f74ef71b0eab781cff0d7ba5f896e3f47
Filipe Bessa <filipe.bessa@caju.com.br>
2025-12-11 13:28:03 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import { SFIcon } from "star-flicks-ds";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="text-center">
         <h1 className="text-4xl font-bold mb-4">Ellen Teixeira</h1>
+        <SFIcon name="star" />
         <p className="text-xl text-gray-600">Odontologia</p>
       </div>
     </main>
-  )
+  );
 }
