a45934370e396b405b6bee49821417af0cd43a75
Filipe Bessa <filipe.bessa@caju.com.br>
2026-03-10 20:41:04 -0300

    chore: upgrade eslint 8 to 9 with flat config
    Migrate from legacy .eslintrc.json to eslint.config.mjs using
    FlatCompat to wrap next/core-web-vitals and prettier extends.

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({ baseDirectory: __dirname });
+
+const config = [
+  ...compat.extends("next/core-web-vitals", "prettier"),
+];
+
+export default config;
